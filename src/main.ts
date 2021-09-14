cd562430e38b0dd64689e4526b41aa37c67bb70a
João Escudero <joaovescudero@gmail.com>
2021-09-13 22:40:15 -0300

    feat(hrm): adding hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { RootModule } from './root.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(RootModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
