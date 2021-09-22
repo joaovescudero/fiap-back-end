69b46eecd56e981c05e4bcf38ecfc6423f02f744
chavesjoabe <chavesjoabe1@gmail.com>
2021-09-21 23:08:31 -0300

    fix: solving conflicts in document column in User Entity

--- src/entites/user.entity.ts
+++ src/entites/user.entity.ts
@@ -25,7 +25,7 @@ export class User {
   @Column({ length: 255 })
   password: string;
 
-  @Column({ length: 255, nullable: true })
+  @Column()
   document: string;
 
   @Column({ nullable: true })
