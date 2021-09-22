005a1c634b837fb27183d653c421c7b88280aa93
chavesjoabe <chavesjoabe1@gmail.com>
2021-09-21 23:03:11 -0300

    fix: solving conflicts in build

--- src/entites/user.entity.ts
+++ src/entites/user.entity.ts
@@ -25,7 +25,7 @@ export class User {
   @Column({ length: 255 })
   password: string;
 
-  @Column({ length: 255, unique: true })
+  @Column({ length: 255, unique: true, nullable: true })
   document: string;
 
   @Column({ nullable: true })
